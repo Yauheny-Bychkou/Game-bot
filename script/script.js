011b204ba832a470e389c0774870872bb794e189
Yauheny Bychkou <375445433672@yandex.by>
2021-08-06 09:35:52 +0300

    ready100percent

--- script/script.js
+++ script/script.js
@@ -1,42 +1,49 @@
 "use strict";
 
 let number;
-let x = Math.floor(Math.random() * 101);
-console.log("x: ", x);
-let count = 10;
+let x;
+let count;
 
 function one(argument) {
+  x = Math.floor(Math.random() * 101);
+  console.log("Первая игра, нужно отгадать число: ", x);
+  count = 10;
   count--;
   function two() {
     number = prompt("Угадай число от 1 до 100");
     if (count === 0) {
       alert("Попытки закончились, хотите сыграть еще?");
-      count = 10;
       x = Math.floor(Math.random() * 101);
-      console.log("x: ", x);
-      one();
+      console.log("Другая игра, нужно отгадать число: ", x);
+      count = 9;
+      two();
     } else if (number === "") {
       alert("Введи число!");
       count++;
-      one();
+      count--;
+      two();
     } else if (number === null) {
       alert("Игра закончена!");
       return;
     } else if (number > x) {
       alert(`Загаданное число больше, осталось попыток: ${count}`);
-      one();
+      count--;
+      two();
     } else if (number < x) {
       alert(`Загаданное число меньше, осталось попыток: ${count}`);
-      one();
+      count--;
+      two();
     } else if (number == x) {
       alert("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
       x = Math.floor(Math.random() * 101);
-      console.log("x: ", x);
-      one();
+      console.log("Другая игра, нужно отгадать число: ", x);
+      count = 9;
+      two();
     } else if (typeof number !== "number") {
       alert("Введи число!");
       count++;
-      one();
+      count--;
+      two();
     }
   }
   two();
